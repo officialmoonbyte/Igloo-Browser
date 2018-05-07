64cf763509becac867d302ada13f0c4ec540d4a4
Alexander Ritter <braydelritter@gmail.com>
2018-05-07 10:02:22 -0500

    Re-organized Program.cs

--- Igloo Browser/Igloo Browser/Program.cs
+++ Igloo Browser/Igloo Browser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CefSharp;
+using Igloo.Connection;
 using Igloo.DNS;
 using Igloo.Engines.CefSharp.Lib;
 using Igloo.Engines.GeckoFx;
@@ -7,14 +8,10 @@ using Igloo.Logger;
 using Igloo.Pages.Browser;
 using Igloo.Resources.lib;
 using Igloo.Server;
-using IndieGoat.InideClient.Default;
-using IndieGoat.Net.SSH;
 using Renci.SshNet.Common;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Net;
 using System.Net.Sockets;
 using System.Reflection;
 using System.Text;
@@ -25,11 +22,17 @@ namespace Igloo
 {
     static class Program
     {
+        #region Vars
+
         //Used to invoke on UI thread
         public static Form InvokeOnUI;
 
         //Server object
         static TcpServer localServer;
+
+        #endregion
+
+        #region Entry Point
 
         /// <summary>
         /// The main entry point for the application.
@@ -37,14 +40,11 @@ namespace Igloo
         [STAThread]
         static void Main()
         {
-            if (File.Exists(@"C:\STOPINTERNETC.cfg"))
-            {
-                Settings.Settings.TryConnection = false;
-            }
-
             //Static vars
             string sshIP = "indiegoat.us";
             int sshPort = 80;
+
+            SetApplicationExceptions(); //Set for logging events when the application shuts down
 
             //Checks if the application exist
             var exists = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location)).Length > 1;
@@ -59,20 +59,54 @@ namespace Igloo
                 //Closing application and client
                 client.Close();
                 Application.Exit();
-                
             }
+
+            InitializeInvokeObject(); //Initialize the invoke object for the start of the application.
 
             //Starting visual and text styles
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(true);
 
-            //Used to invoke on UI thread
-            InvokeOnUI = new Form();
-            InvokeOnUI.Size = new System.Drawing.Size(0, 0);
-            InvokeOnUI.FormBorderStyle = FormBorderStyle.None;
-            InvokeOnUI.Show();
-            InvokeOnUI.Hide();
+            //Check if there is an active internet connection
+            if (!CheckInternet.CheckForInternetConnection())
+            {
+                ILogger.AddToLog("ERROR", "Connection to the internet was not found!");
+                MessageBox.Show("Connection to the internet was not found! this browser is currently closing.", ResourceInformation.ApplicationName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
 
+            InitializeSSH(); //Attemps to connect to the SSH server.
+            InitializeLocalServer(); //Loads the local server used to open a new form when a new application is open.
+
+            //Initialize CefSharp
+            VoidCef.InitializeCefSharp();
+
+            //Initialize GeckoFx
+            VoidGecko.InitializeGecko();
+
+            InitializeStripe(); //Initialize the stripe API
+
+            ILogger.AddToLog(ResourceInformation.ApplicationName, "Running main form now!");
+
+            //Initializing History
+            IHistory.LoadFromFile();
+
+            OpenForm(); //Opens a new borwser window
+            StartFormTimer(); //Starts a timer to check when no forms are open
+
+            //Running the application loop.
+            Application.Run();
+        }
+
+        #endregion
+
+        #region Application Exceptions
+
+        /// <summary>
+        /// Sets the events of application exceptions.
+        /// </summary>
+        static void SetApplicationExceptions()
+        {
             //App doamin and application events for logging.
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
             Application.ApplicationExit += Application_ApplicationExit;
@@ -81,91 +115,79 @@ namespace Igloo
 
             ILogger.AddToLog("INFO", "Set handle's for all application exceptions's");
 
+        }
 
-            //Check if there is an active internet connection
-            if (!CheckInternet.CheckForInternetConnection())
-            {
-                ILogger.AddToLog("ERROR", "Connection to the internet was not found!");
-                MessageBox.Show("Connection to the internet was not found! thi sbrowser is currently closing.", ResourceInformation.ApplicationName, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Application.Exit();
-            }
+        #endregion
 
-            //Checks if the network should connect to the ssh server
-            if (false)
-            {
-                Thread sshThread = new Thread(new ThreadStart(() =>
-                {
+        #region Invoke Object
 
-                    //Sets the VortexStudio ip and the External IP
-                    var vortexStudioIP = Dns.GetHostAddresses(sshIP)[0];
-                    string externalIP = new WebClient().DownloadString("http://icanhazip.com/"); externalIP = externalIP.Trim();
+        /// <summary>
+        /// Creates and initialize a new Invoke object used for multi threading threads.
+        /// </summary>
+        static void InitializeInvokeObject()
+        {
+            //Used to invoke on UI thread
+            InvokeOnUI = new Form();
+            InvokeOnUI.Size = new System.Drawing.Size(0, 0);
+            InvokeOnUI.FormBorderStyle = FormBorderStyle.None;
+            InvokeOnUI.Show();
+            InvokeOnUI.Hide();
+        }
 
-                    //Changes the IP of the SSH client, if using local internet.
-                    if (vortexStudioIP.ToString() == externalIP)
-                    {
-                        sshIP = "192.168.0.8";
-                        ILogger.AddToLog("INFO", "Detected local network use! Changing IP of the SSH server. External IP " + externalIP + ", Vortex Studio IP : " + vortexStudioIP.ToString());
-                    }
+        #endregion
 
-                    ILogger.AddToLog("SSH", "Trying to connect to SSH server.... Please wait.");
+        #region Networking startup task
 
-                    //Connects to the ssh server
-                    GlobalSSH sshService = new GlobalSSH(); ILogger.AddToLog("SSH", "Created SSH object");
-                    sshService.StartSSHService(sshIP, sshPort.ToString(), "public", "Public36");
+        /// <summary>
+        /// Initialize a new SSH connection
+        /// </summary>
+        static void InitializeSSH()
+        {
+            //Starts the ssh connection task
+            bool ConnectToSSH = true;
+            bool ConnectToDynUpdate = true;
+            bool ConnectToUniversalServer = true;
+            new Thread(new ThreadStart(() => { new ServerConnections(ConnectToSSH, ConnectToDynUpdate, ConnectToUniversalServer); }));
+        }
 
-                    ILogger.AddToLog("SSH", "SSH service started!");
-                     
-                    Thread.Sleep(1000);
-
-                    //Forwards all local port's for the server
-                    sshService.ForwardLocalPort("2445", "192.168.0.11"); ILogger.AddToLog("SSH", "Forward Port 2445");
-                    sshService.ForwardLocalPort("3389", "192.168.0.11"); ILogger.AddToLog("SSH", "Forward Port 3389");
-                    sshService.ForwardLocalPort("5750", "192.168.0.11"); ILogger.AddToLog("SSH", "Forward Port 5750");
-
-                    //Initializing update
-                    //DynUpdater.Main dynUpdater = new DynUpdater.Main();
-                    //dynUpdater.UpdateURLLocation = "https://dl.dropboxusercontent.com/s/9z2xx5rbi1qw4vw/Install.zip?dl=0";
-                    //dynUpdater.CheckUpdate("127.0.0.1", 5750);
-
-                    ILogger.AddToLog("DynUpdater", "Initialized DYNUpdater on port 5750.");
-
-                    //Connects to the Universal server
-                    Settings.Settings.UniversalConnection = new IndieClient();
-                    Settings.Settings.UniversalConnection.ConnectToRemoteServer("localhost", 2445);
-                    ILogger.AddToLog("Indie Client", "Connected to host localhost:2445");
-
-                })); sshThread.Start();
-            }
-
+        /// <summary>
+        /// Starts listening on a local port to detect when the application is open -- Saves memory.
+        /// </summary>
+        static void InitializeLocalServer()
+        {
             //Starts the local tcp server
             localServer = new TcpServer(); localServer.StartServer();
             ILogger.AddToLog(ResourceInformation.ApplicationName, "Finished initializing TCP Server.");
+        }
 
-            //Initialize CefSharp
-            VoidCef.InitializeCefSharp();
+        #endregion
 
-            //Initialize GeckoFx
-            VoidGecko.InitializeGecko();
+        #region Stripe Initialization
 
-            //Set the IP and PORT of the Proxy Server
-            string ip = "localhost";
-            int iport = 5567;
-
+        /// <summary>
+        /// Initializes stripe API
+        /// </summary>
+        static void InitializeStripe()
+        {
             //Instalization for Stripe
             Stripe.StripeConfiguration.SetApiKey("sk_live_gTIkgQoj5jZGrpGJ7vj3x4Mt");
             ILogger.AddToLog("Stripe", "Instalization complete!");
 
-            ILogger.AddToLog(ResourceInformation.ApplicationName, "Running main form now!");
+            //**Stripe is currently not being used in this application. This thead is meant for testing. And as soon as we figure
+            //Out how to store the API key. We will not be using STRIPE to accept donations. Except we are moving from STRIPE to
+            //Patreon. As feedback from our open reddit AMA. We do agree, keeping a private API key public is not secure.
+        }
 
-            //Declaring new BrowserWindow
-            BrowserWindow browser = new BrowserWindow();
+        #endregion
 
-            //Initializing History
-            IHistory.LoadFromFile();
+        #region Starting main form
 
-            //Showing the browser form.
-            browser.Show();
-
+        /// <summary>
+        /// Starts up a timer to check if there are any forms open.
+        /// If there are no forms open, closes the application
+        /// </summary>
+        static void StartFormTimer()
+        {
             //Setting up the timer
             new Thread(new ThreadStart(() =>
             {
@@ -191,10 +213,21 @@ namespace Igloo
                     Thread.Sleep(2500);
                 }
             })).Start();
-
-            //Running the application loop.
-            Application.Run();
         }
+
+        /// <summary>
+        /// Loads a new main window.
+        /// </summary>
+        static void OpenForm()
+        {
+            //Declaring new BrowserWindow
+            BrowserWindow browser = new BrowserWindow();
+
+            //Showing the browser form.
+            browser.Show();
+        }
+
+        #endregion
 
         #region Application Logging
 
@@ -298,6 +331,5 @@ namespace Igloo
         }
 
         #endregion
-
     }
 }
